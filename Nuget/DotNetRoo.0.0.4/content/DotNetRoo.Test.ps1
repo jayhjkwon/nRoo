0a6e57f9c4eeed1d6e24936f144790b55d8f22f4
Ray H.J Kwon <nicekhj@gmail.com>
2011-12-28 20:54:55 +0900

    added sample customscaffold for activerecord

---
+++ Nuget/DotNetRoo.0.0.4/content/DotNetRoo.Test.ps1
@@ -0,0 +1,7 @@
+﻿[T4Scaffolding.Scaffolder(Description = "My Test Scaffold")][CmdletBinding()]
+param(        
+    [string]$Project,
+	[string]$CodeLanguage,
+	[string[]]$TemplateFolders,
+	[switch]$Force = $false
+)
---
+++ Nuget/DotNetRoo.0.0.6/content/CodeTemplates/Scaffolders/DotNetRoo.ActiveRecord/DotNetRoo.ActiveRecord.ps1
@@ -0,0 +1,20 @@
+[T4Scaffolding.Scaffolder(Description = "DotNetRoo ActiveRecord Scaffold")][CmdletBinding()]
+param(        
+    [string]$Project,
+	[string]$CodeLanguage,
+	[string[]]$TemplateFolders,
+	[switch]$Force = $false
+)
+
+#Add Castle ActiveRecord Library from Nuget
+Write-Host Installing : EntityFramework
+Install-Package Castle.Activerecord
+
+
+$outputPath = "ExampleOutput"  # The filename extension will be added based on the template's <#@ Output Extension="..." #> directive
+$namespace = (Get-Project $Project).Properties.Item("DefaultNamespace").Value
+
+Add-ProjectItemViaTemplate $outputPath -Template DotNetRoo.ActiveRecord.Template `
+	-Model @{ Namespace = $namespace; ExampleValue = "Hello, world!" } `
+	-SuccessMessage "Added TestScaffold output at {0}" `
+	-TemplateFolders $TemplateFolders -Project $Project -CodeLanguage $CodeLanguage -Force:$Force
