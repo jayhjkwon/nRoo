dca567ddba5af08fa94734b2ee37fe9135b11746
Ray H.J Kwon <nicekhj@gmail.com>
2012-01-09 17:54:16 +0900

    deleted folders enter the commit message for your changes. Lines starting

--- Nuget/DotNetRoo.0.0.4/content/DotNetRoo.Test.ps1
+++
@@ -1,7 +0,0 @@
-﻿[T4Scaffolding.Scaffolder(Description = "My Test Scaffold")][CmdletBinding()]
-param(        
-    [string]$Project,
-	[string]$CodeLanguage,
-	[string[]]$TemplateFolders,
-	[switch]$Force = $false
-)
